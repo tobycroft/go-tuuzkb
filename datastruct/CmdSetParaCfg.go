33f946549109d8196ef2ab04245446d0f05ac89b
tuuz <silverhawk@foxmail.com>
2024-08-30 18:44:48 +0800

    TuuzPC

--- datastruct/CmdSetParaCfg.go
+++ datastruct/CmdSetParaCfg.go
@@ -7,6 +7,23 @@ import (
 	"main.go/define/cmd"
 )
 
+type para struct {
+	Mode                 byte
+	Cfg                  byte
+	ComAddress           byte //字节窗口通信地址
+	BaudRate             byte //4 个字节芯片串口通信波特率，高字节在前，默认为 0x00002580，即波特率为 9600bps
+	Blank1               uint16
+	SepDelay             uint16
+	PidVid               uint32
+	KeyboardDelay        byte
+	KeyboardReleaseDelay byte
+	EnterSignAuto        byte
+	EnterSign            uint64 //8个字节
+	KeyboardFilter       uint64
+	FastUploadSign       byte
+	Blank2               [12]byte
+}
+
 func (kb *Kb) CmdSetParaCfg() *Kb {
 	kb.head()
 	kb.Ctx.Cmd = cmd.CMD_SET_PARA_CFG
