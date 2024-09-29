2b06113597729fbee7e2a48e025bbd4a2153c72c
tuuz <silverhawk@foxmail.com>
2024-09-29 23:56:53 +0800

    TuuzPC

---
+++ netSender/CmdSendKbGeneralData.go
@@ -0,0 +1,11 @@
+package ttlSender
+
+import "main.go/define/cmd"
+
+// 发送 USB 键盘普通数据
+func (kb *NetSender) CmdSendKbGeneralData() *NetSender {
+	kb.head()
+	kb.Ctx.Cmd = cmd.CMD_SEND_KB_GENERAL_DATA
+	kb.data([]byte{}).send()
+	return kb
+}
