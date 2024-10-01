14f82cae16dc235d0656a43b3bcc44aea3d944d1
tuuz <silverhawk@foxmail.com>
2024-10-01 18:04:15 +0800

    TuuzPC

--- netSender/KeyboardData.go
+++ netSender/KeyboardData.go
@@ -1,7 +1,18 @@
 package netSender
 
+//	type KeyboardData struct {
+//		Ctrl   byte
+//		Resv   byte
+//		Button [6]byte
+//	}
+
 type KeyboardData struct {
-	Ctrl   byte
-	Resv   byte
-	Button [6]byte
+	Ctrl    byte
+	Resv    byte
+	Button1 byte
+	Button2 byte
+	Button3 byte
+	Button4 byte
+	Button5 byte
+	Button6 byte
 }
