cfe499988fe946d6a420f91b905b811a5224bc20
tuuz <silverhawk@foxmail.com>
2024-10-01 18:14:17 +0800

    TuuzPC

---
+++ action/MainRun.go
@@ -0,0 +1,24 @@
+package action
+
+import (
+	"main.go/netReceiver"
+	"main.go/netSender"
+	"sync"
+)
+
+type Action struct {
+	//将你需要缓存的数据存在这里
+	ClientRx *netReceiver.ClientRx
+	ClientTx *netSender.ClientTx
+
+	Button  sync.Map
+	MaskKey []byte
+}
+
+func (self *Action) MainRun(clientrx *netReceiver.ClientRx, clienttx *netSender.ClientTx) {
+	self.ClientRx = clientrx
+	self.ClientTx = clienttx
+	go self.mouse_runnable()
+	self.keyboard_runnable()
+	panic("runnable")
+}
