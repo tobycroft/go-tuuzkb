e72c28de13631dd1e35306e5d4fb16acafdbdc71
tuuz <silverhawk@foxmail.com>
2024-10-01 16:32:01 +0800

    TuuzPC

--- main.go
+++ main.go
@@ -9,13 +9,7 @@ import (
 )
 
 func main() {
-	//10.0.0.90
-	//rx := netReceiver.ClientRx{}
-	//rx.IP = "10.0.0.91"
-	//rx.Port = 6666
-	//rx.Run()
-	//var run action.Runnable
-	//go run.MainRun(&rx, &tx)
+
 	ClientTx := &netSender.ClientTx{}
 	ClientRx := &netReceiver.ClientRx{}
 	ClientRx.Ready()
