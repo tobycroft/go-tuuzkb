58c75bd6dd57620b5c22c38be15bd1fb1bd579d8
tuuz <silverhawk@foxmail.com>
2024-10-05 15:32:41 +0800

    TuuzPC

--- route/v1/indexRouter.go
+++ route/v1/indexRouter.go
@@ -2,7 +2,6 @@ package v1
 
 import (
 	"github.com/gin-gonic/gin"
-	"main.go/app/v1/index/controller"
 )
 
 func IndexRouter(route *gin.RouterGroup) {
@@ -10,6 +9,4 @@ func IndexRouter(route *gin.RouterGroup) {
 		context.String(0, route.BasePath())
 	})
 
-	controller.IndexController(route.Group("index"))
-	controller.LoginController(route.Group("login"))
 }
