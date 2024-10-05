674454a78a79dcd01c96c0edc021b2c1cb20b250
tuuz <silverhawk@foxmail.com>
2024-10-05 14:57:25 +0800

    TuuzPC

--- action/Keyboard.go
+++ action/Keyboard.go
@@ -14,58 +14,13 @@ func (self *Action) keyboard_runnable() {
 		go self.kb_banSomeKeys(c)
 		go self.kb_reboot(c)
 		fmt.Println("keybaordrecv", c)
-		self.ClientTx.CmdSendKbGeneralDataRaw(self.kb_gen_output(c))
+		out := self.kb_gen_output(c)
+		if out.Resv != self.lastPress {
+			self.ClientTx.CmdSendKbGeneralDataRaw(out)
+		}
 
 	}
 	panic("键盘通道意外结束")
-}
-
-func (self *Action) kb_washing(c netSender.KeyboardData) (Ctrl byte, Button [6]byte, sum byte) {
-	_, ok := self.Mask.Button.Load(c.Button0)
-	if !ok {
-		Button[0] = c.Button0
-	}
-	sum += Button[0]
-	_, ok = self.Mask.Button.Load(c.Button1)
-	if !ok {
-		Button[1] = c.Button1
-	}
-	sum += Button[1]
-	_, ok = self.Mask.Button.Load(c.Button2)
-	if !ok {
-		Button[2] = c.Button2
-	}
-	sum += Button[2]
-	_, ok = self.Mask.Button.Load(c.Button3)
-	if !ok {
-		Button[3] = c.Button3
-	}
-	sum += Button[3]
-	_, ok = self.Mask.Button.Load(c.Button4)
-	if !ok {
-		Button[4] = c.Button4
-	}
-	sum += Button[4]
-	_, ok = self.Mask.Button.Load(c.Button5)
-	if !ok {
-		Button[5] = c.Button5
-	}
-	sum += Button[5]
-	self.ClientRx.OriginCtrl.Range(func(key, value interface{}) bool {
-		_, ok = self.Mask.Ctrl.Load(key.(byte))
-		if !ok {
-			Ctrl += key.(byte)
-		}
-		return true
-	})
-	sum += Ctrl
-	return
-}
-
-func (self *Action) kb_gen_output(c netSender.KeyboardData) (out netSender.KeyboardData2) {
-	out.Ctrl, out.Button, out.Resv = self.kb_washing(c)
-	fmt.Println("keybaordsnd", out)
-	return out
 }
 
 func (self *Action) kb_actvate(c netSender.KeyboardData) {
--- action/MainFunc.go
+++ action/MainFunc.go
@@ -1,6 +1,15 @@
 package action
 
-import "main.go/netSender"
+import (
+	"fmt"
+	"main.go/netSender"
+)
+
+func (self *Action) kb_gen_output(c netSender.KeyboardData) (out netSender.KeyboardData2) {
+	out.Ctrl, out.Button, out.Resv = self.kb_washing(c)
+	fmt.Println("keybaordsnd", out)
+	return out
+}
 
 func (self *Action) checkKeyIsPressed(c netSender.KeyboardData, Ctrl byte, Btn ...byte) bool {
 	num := 0
@@ -29,4 +38,45 @@ func (self *Action) checkKeyIsPressed(c netSender.KeyboardData, Ctrl byte, Btn .
 	} else {
 		return false
 	}
+}
+func (self *Action) kb_washing(c netSender.KeyboardData) (Ctrl byte, Button [6]byte, sum byte) {
+	_, ok := self.Mask.Button.Load(c.Button0)
+	if !ok {
+		Button[0] = c.Button0
+	}
+	sum += Button[0]
+	_, ok = self.Mask.Button.Load(c.Button1)
+	if !ok {
+		Button[1] = c.Button1
+	}
+	sum += Button[1]
+	_, ok = self.Mask.Button.Load(c.Button2)
+	if !ok {
+		Button[2] = c.Button2
+	}
+	sum += Button[2]
+	_, ok = self.Mask.Button.Load(c.Button3)
+	if !ok {
+		Button[3] = c.Button3
+	}
+	sum += Button[3]
+	_, ok = self.Mask.Button.Load(c.Button4)
+	if !ok {
+		Button[4] = c.Button4
+	}
+	sum += Button[4]
+	_, ok = self.Mask.Button.Load(c.Button5)
+	if !ok {
+		Button[5] = c.Button5
+	}
+	sum += Button[5]
+	self.ClientRx.OriginCtrl.Range(func(key, value interface{}) bool {
+		_, ok = self.Mask.Ctrl.Load(key.(byte))
+		if !ok {
+			Ctrl += key.(byte)
+		}
+		return true
+	})
+	sum += Ctrl
+	return
 }
