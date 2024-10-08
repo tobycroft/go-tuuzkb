1dc6486d395fbf6eae867b56764e83417d3a28cf
tuuz <silverhawk@foxmail.com>
2024-10-09 00:31:04 +0800

    TuuzPC

--- netSender/ClientTx.go
+++ netSender/ClientTx.go
@@ -9,8 +9,6 @@ import (
 var Ctx = &ClientTx{}
 
 type ClientTx struct {
-	sendData sendData
-
 	TxChannel      chan []byte
 	MouseTxChannel chan any
 }
@@ -25,33 +23,39 @@ const start2 = 0xab
 
 // 发送数据联合体
 type sendData struct {
-	Head    uint16 // 帧头 (2个字节)
-	Addr    byte   // 地址码 (1个字节)
-	Cmd     byte   // 命令码 (1个字节)
-	Len     byte   // 后续数据长度 (1个字节)
-	sendBuf *bytes.Buffer
-	ctx     *ClientTx
+	Head uint16 // 帧头 (2个字节)
+	Addr byte   // 地址码 (1个字节)
+	Cmd  byte   // 命令码 (1个字节)
+	Len  byte   // 后续数据长度 (1个字节)
 }
 
-func (self *ClientTx) head(Cmd byte) *sendData {
-	return &sendData{
-		Head:    uint16(start1)<<8 | uint16(start2),
-		Addr:    0x00,
-		Cmd:     Cmd,
-		Len:     0x00,
+type snd struct {
+	sendBuf  *bytes.Buffer
+	ctx      *ClientTx
+	sendData *sendData
+}
+
+func (self *ClientTx) head(Cmd byte) *snd {
+	return &snd{
+		sendData: &sendData{
+			Head: uint16(start1)<<8 | uint16(start2),
+			Addr: 0x00,
+			Cmd:  Cmd,
+			Len:  0x00,
+		},
 		ctx:     self,
 		sendBuf: &bytes.Buffer{},
 	}
 }
 
-func (self *sendData) data(data any) *sendData {
+func (self *snd) data(data any) *snd {
 	bb := bytes.Buffer{}
 	err := binary.Write(&bb, binary.BigEndian, data)
 	if err != nil {
 		panic(fmt.Sprintln("binary编译失败", err))
 	}
-	self.Len = self.Len + byte(bb.Len())
-	err = binary.Write(self.sendBuf, binary.BigEndian, self)
+	self.sendData.Len = self.sendData.Len + byte(bb.Len())
+	err = binary.Write(self.sendBuf, binary.BigEndian, self.sendData)
 	if err != nil {
 		panic(fmt.Sprintln("binary编译失败", err))
 	}
@@ -59,7 +63,7 @@ func (self *sendData) data(data any) *sendData {
 	return self
 }
 
-func (self *sendData) sum() *sendData {
+func (self *snd) sum() *snd {
 	sum := byte(0x00)
 	for _, b := range self.sendBuf.Bytes() {
 		sum = sum + (b)
@@ -71,7 +75,7 @@ func (self *sendData) sum() *sendData {
 	return self
 }
 
-func (self *sendData) send() {
+func (self *snd) send() {
 	self.sum()
 	self.ctx.TxChannel <- self.sendBuf.Bytes()
 	//time.Sleep(1 * time.Millisecond)
