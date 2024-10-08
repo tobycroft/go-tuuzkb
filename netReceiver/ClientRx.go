29e04a5ff7349c7fb93366bf04537074a2b4893d
tuuz <silverhawk@foxmail.com>
2024-10-08 18:19:02 +0800

    TuuzPC

--- netReceiver/ClientRx.go
+++ netReceiver/ClientRx.go
@@ -118,6 +118,10 @@ func (self *ClientRx) Router9239(Data []byte, Addr net.Addr, PackConn net.Packet
 		netSender.CmdGetUsbStringRecv(Data)
 		break
 
+	case 0xca:
+		fmt.Println("错误：", hex.EncodeToString(Data[2:2]))
+		break
+
 	default:
 		go fmt.Println("rcv_unreco:", hex.EncodeToString(Data))
 
