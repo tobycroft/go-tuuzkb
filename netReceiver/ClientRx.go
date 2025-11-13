bec0a0bb037727abb1b2cb89f06a3cd4e9ed97dc
Tuuz <2539@tuuz.cn>
2025-11-13 19:21:22 +0800

    Refactor UDP packet handling with sync.Pool and zero-copy slicing

--- netReceiver/ClientRx.go
+++ netReceiver/ClientRx.go
@@ -87,6 +87,7 @@ func (self *ClientRx) MessageRouter(Data []byte) {
 		break
 
 	case 0x88:
+		fmt.Println(Data)
 		////fmt.Println("kff", len(Data), Data[1], Data[2], 2+int(Data[1]))
 		//frame := keyframe{
 		//	DataLength: Data[1],
